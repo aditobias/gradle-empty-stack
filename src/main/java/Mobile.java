93dbe3b5a423389b3b85a6fd149aaae53cf72685
aditobias <40589504+aditobias@users.noreply.github.com>
2019-10-10 13:36:07 +0800

    Completed Story 2

--- src/main/java/Mobile.java
+++ src/main/java/Mobile.java
@@ -3,7 +3,35 @@ public class Mobile {
     private String color;
     private String brand;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     public void makeCall(String message){
         System.out.println("Message: " + message);
+    }
+
+    public void basicInfo(){
+        System.out.println("name : " + getName() + ", color : " + getColor() + ", brand : " + getBrand());
     }
 }
